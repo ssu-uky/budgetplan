c9cd4e56c63b3f190907902297ce506b9a53e73a
ssu-uky <id_suhyun@naver.com>
2023-12-05 15:51:38 +0900

    💡 chore : 불필요한 부분 삭제

--- plans/urls.py
+++ plans/urls.py
@@ -5,7 +5,6 @@ from . import views
 urlpatterns = [
     path("monthly/", views.MonthlyPlanView.as_view()),
     path("monthly/<str:owner>/", views.MonthlyPlanDetailView.as_view()),
-    path("today/<str:owner>/", views.TodayView.as_view()),
     path(
         "<str:owner>/<int:year>-<int:month>-<int:day>/", views.TodayPlanView.as_view()
     ),
