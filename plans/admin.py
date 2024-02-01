34e0265814aa34b2799249487d3d0586e88a13a5
ssu-uky <id_suhyun@naver.com>
2024-02-01 17:26:08 +0900

    planadmin 추가, erd 업로드, requirement 파일 추가

--- plans/admin.py
+++ plans/admin.py
@@ -1,3 +1,27 @@
 from django.contrib import admin
+from .models import MonthlyPlan, TodayPlan
 
-# Register your models here.
+
+@admin.register(MonthlyPlan)
+class MonthlyPlanAdmin(admin.ModelAdmin):
+    list_display = (
+        "pk",
+        "owner",
+        "monthly_income",
+        "monthly_saving",
+        "monthly_total_spgit ending",
+        "monthly_possible",
+    )
+    list_display_links = ("pk", "owner")
+
+
+@admin.register(TodayPlan)
+class TodayPlanAdmin(admin.ModelAdmin):
+    list_display = (
+        "pk",
+        "owner",
+        "today_total_spending",
+        "today_possible",
+        "date",
+    )
+    list_display_links = ("pk", "owner")
